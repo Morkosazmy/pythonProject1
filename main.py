8128c6bf72b71fc49b1c041035ae8307697c3d56
morkosazmy <azmymorkos@cy-tech.fr>
2024-11-11 20:02:09 +0100

    N°18

--- main.py
+++ main.py
@@ -1,41 +1,22 @@
 # mini calculator
 
 import math
+import time
+from asyncio import wait_for
 
-'''vid 17 : interest rate over the years'''
-total = float
-rate = float
-time = float
-initial = float
+'''vid 18 : for loops'''
 
-while True:
-    initial = float(input("Enter the initial amount of money : "))
-    if initial < 0:
-        print("The initial value cant be a negative number !")
-        initial = float(input("Enter the initial amount of money again : "))
+credit_card = "1234-5678-9012-3456"
+
+for x in credit_card:
+    if credit_card[4] == x or credit_card[9] == x or credit_card[14] == x:
+        continue
     else:
-        break
+        print(x)
 
-while True:
-    rate = float(input("Enter the rate of interest in percentage (numbers only) : "))
-    if rate < 0:
-        print("The rate of interest cant be a negative number !")
-        rate = float(input("Re-enter the rate of interest : "))
-    else:
-        break
+for x in range(1,11):
+    print(x)
 
-while True:
-    time = int(input("Enter the number of years for this investement : "))
-    if time < 0:
-        print("The time cant be a negative value !")
-        time = int(input("Re-enter the time of years : "))
-    else:
-        break
-total = initial * pow((1 + ( rate / 100 )),time)
-print(f"Your initial total was : ${initial}")
-print(f"The rate of interest is : {rate}%")
-print(f"The number of years is : {time} year(s).")
-profit = total - initial
-
-print(f"Your balance after {time} year(s) is : ${total:.2f}")
-print(f"Your profit is : ${profit}")
+#equivalent of
+for x in range(0,10):
+    print(x+1)
