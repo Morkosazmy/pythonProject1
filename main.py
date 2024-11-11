148c5d503234256a9fa2b9ce71cc1d92a44ec96d
morkosazmy <azmymorkos@cy-tech.fr>
2024-11-11 20:13:32 +0100

    N°19

--- main.py
+++ main.py
@@ -4,19 +4,26 @@ import math
 import time
 from asyncio import wait_for
 
-'''vid 18 : for loops'''
+'''vid 19 : nested loops'''
 
 credit_card = "1234-5678-9012-3456"
 
-for x in credit_card:
-    if credit_card[4] == x or credit_card[9] == x or credit_card[14] == x:
-        continue
-    else:
-        print(x)
+for y in range (3):
+    print()
+    for x in range (1,10):
+        print(x, end="")
+    print()
 
-for x in range(1,11):
-    print(x)
+rows = int(input("Enter the number of rows: "))
+columns = int(input("Enter the number of columns: "))
+symbol = input("Enter a symbol: ")
 
-#equivalent of
-for x in range(0,10):
-    print(x+1)
+for y in range(rows):
+    for x in range(columns):
+        print(symbol, end="")
+    print()
+
+
+
+for i in range(4): # equivalent of range(0,4)
+    print(i)
