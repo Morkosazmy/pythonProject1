7288be632d1683fd2536933337027224d21da5be
morkosazmy <azmymorkos@cy-tech.fr>
2024-11-12 00:51:28 +0100

    N°22

--- main.py
+++ main.py
@@ -2,41 +2,28 @@ import math
 import time
 from itertools import count
 
-'''vid 21: List, set, tuple'''
-#List = []
-#Set = {}
-#Tuple = ()
+'''vid 22: Shopping cart ! '''
 
-"""LISTS: Ordered and changeable ! (Can have duplicates) """
-games = ["God of war", "Uncharted", "The last of us", "Ghost of Tsushima"] #List
-print(games)
+total = 0
+prices = []
+items = []
 
-games.append("Ratchet and Clank")
-games.append("Ratchet and Clank")
-games.append("Ratchet and Clank")
-games.append("Ratchet and Clank")
-games.insert(4,"Elden ring")
-for game in games:
-    print(f"-{game}")
-games.sort()
+while True:
+    item = input("Enter the name of the item that you would like to purchase please (q to quit) : ")
+    if item.lower() == "q":
+        break
+    else:
+        items.append(item)
+        price = float(input(f"Enter the price of a {item} please: $"))
+        prices.append(price)
+
+print("----- YOUR SHOPPING CART -----")
+for item in range(0,len(items)-1):
+    print(items[item],end=", ")
+for item in range(len(items)-1,len(items)):
+    print(items[item],end=".\n")
 print()
-for game in games:
-    print(f"-{game}")
-print(games.count("Ratchet and Clank"))
+for price in prices:
+    total += price
 
-
-"""SETS: Unordered and immutable(cant change), (Can't have duplicates) , We can add or remove though ! """
-shapes = {"Circle", "Rectangle", "Square", "Cylinder","Circle"} # duplicates won't count when printing
-print(shapes)
-print("Circle" in shapes)
-
-
-"""TUPLES: Ordered but unchangeable Faster than a List, Duplicates are ok"""
-fruits = ("Mango", "Banana", "Orange", "Apple", "Strawberry", "Mango")
-print(fruits)
-print(fruits.index("Mango",1))
-print(fruits)
-for fruit in fruits:
-    print(f"-{fruit}")
-print(fruits.count("Mango"))
-print(fruits.count("Apple"))
+print(f"Your total is : ${total}")
