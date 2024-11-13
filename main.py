6a548e99a9c8a4b3e27dfb161b8eed7239a35727
morkosazmy <azmymorkos@cy-tech.fr>
2024-11-13 23:05:47 +0100

    N°29 Rock Paper Scissor

--- main.py
+++ main.py
@@ -1,46 +1,29 @@
 import random
+from time import process_time_ns
 
-dice = random.randint(1,6)
-print(dice)
+#ROCK PAPER SCISSOR
 
-options = ["rock","paper","scissors"]
+print("Welcome to the rock paper scissor game ! ")
 
-option = random.choice(options)
-print(option)
-
-# we can shuffle as well !
-
-cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"] #in order to shuffle it has to be a List
-# a Tuple won't work with the shuffle method
-
-
-random.shuffle(cards)
-print(cards)
-
-#mini guessing game !
-
-guesses = 0
-
-low = 1
-high = 100
-
-random_number = random.randint(low, high)
-
-while True:
-    guess = input("Guess a number between 1 & 100: ")
-    #guesses += 1
-    if not guess.isdigit():
-        print("Please insert an actual number between 1 and 100")
-    elif int(guess) < 1 or int(guess) >100:
-        print("Number is out of range, please insert a number between 1 and 100")
-    elif int(guess) > random_number:
-        print(f"your guess ( {guess} ) is too high")
-        guesses += 1
-    elif int(guess) < random_number:
-        print(f"your guess ( {guess} ) is too low")
-        guesses += 1
+options = ["rock","paper","scissor"]
+playing = True
+while playing:
+    computer = random.choice(options)
+    player = input("Choose (rock, paper, scissor): ").lower()
+    if player not in options:
+        print("Invalid choice please chose again ! ")
+    elif player == computer:
+        print("Tie")
+    elif player == "rock" and computer == "scissor" or player == "paper" and computer == "rock" or player == "scissor" and computer == "paper":
+        print("You win ! ")
     else:
-        guesses += 1
-        print(f"({guess}) is the correct answer !")
+        print("You lose ! ")
+    print(f"you choose : {player}")
+    print(f"computer picked : {computer}")
+    if not input("Would you like to play again ? (y/n) ").lower() == "y":
+        playing = False
         break
-print(f"This round took you {guesses} guesses !")
+    # elif player == "rock" and computer == "scissor":
+    # elif player == "rock" and computer == "scissor":
+print("Game Over ! ")
+print("Thanks for playing ! ")
