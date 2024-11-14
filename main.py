050e03b2cf91904df16669b117f7e582f5d06e1a
morkosazmy <azmymorkos@cy-tech.fr>
2024-11-14 22:54:25 +0100

    N°37 Membership operators

--- main.py
+++ main.py
@@ -1,36 +1,31 @@
-#ITERABLES
+#membership operators
+from itertools import count
 
-numbers = [1, 2, 3, 4, 5]
-letters = ["a", "b", "c", "d", "e"]
-extra_numbers = (1, 2, 3, 4, 5)
-fruits = {"Apple", "Orange", "Banana", "Pineapple", "Coconut"}
-name = "full name"
-my_dictionary = {"Morkos": 1, "Mark": 2, "Michael": 3, "Momo": 4}
+word = "APPLE"
 
+letter = input("Guess the letter in the secret word : ")
 
-for number in numbers:
-    print(number)
-print()
+if letter in word:
+    amount = word.count(letter)
+    print(f"The letter {letter} exists in the secret message {amount} times !")
+else:
+    print(f"The letter {letter} does NOT exist in the secret message !")
 
-for letter in letters:
-    print(letter)
-print()
-for extra_number in extra_numbers:
-    print(extra_number)
-print()
+grades = {"sandy" : "C",
+          "spongebob" : "B",
+          "squidward" : "A",
+          "patrick" : "D",
+          "plankton" : "F"}
 
-for fruit in fruits:
-    print(fruit)
-print()
-for letter in name:
-    print(letter, end="")
-print()
-print()
+name = input("Enter the name for which the grade is desired : ")
+if name not in grades:
+    print("There was no student with that name ! ")
+else:
+    print(f"the student {name} got the grade {grades[name]}")
 
-for key in my_dictionary.keys():
-    print(key)
-for value in my_dictionary.values():
-    print(value)
-for key, value in my_dictionary.items():
-    print(f"{key} : {value}")
-    print(key,value)
+email = input("Insert an email : ")
+
+if "@" in email and "." in email:
+    print("Valid email")
+else:
+    print("Invalid email")
