9635e7b2d9afac60bea4f0ade7d7359c7bf6e805
morkosazmy <azmymorkos@cy-tech.fr>
2024-11-14 21:58:00 +0100

    N°36 ITERABLES

--- main.py
+++ main.py
@@ -1,32 +1,36 @@
-import time
+#ITERABLES
+
+numbers = [1, 2, 3, 4, 5]
+letters = ["a", "b", "c", "d", "e"]
+extra_numbers = (1, 2, 3, 4, 5)
+fruits = {"Apple", "Orange", "Banana", "Pineapple", "Coconut"}
+name = "full name"
+my_dictionary = {"Morkos": 1, "Mark": 2, "Michael": 3, "Momo": 4}
 
 
-def total(*args):
-    total = 0
-    for arg in args:
-        total += arg
-    return total
-#print(total(1,2,3,4,5))
-
-def address(**kwargs):
-    address = ""
-    for key, value in kwargs.items():
-        print(f"{key} : {value}")
-
-#address(street_no = "123", street_name = "rue de blabla", commune = "Paris", pays = "France")
-def shipping_label(*args, **kwargs):
-    for arg in args:
-        print(arg, end=" ")
-    print()
-   # for value in kwargs.values(): # we can use for key, value in kwargs.items() as well !
-    print(f"{kwargs.get('street_no')} {kwargs.get('street_name')}")
-    if "code_postal" in kwargs:
-        print(f"{kwargs.get('code_postal')} {kwargs.get('commune')} {kwargs.get('pays')}")
-    else:
-        print(f"{kwargs.get('commune')} {kwargs.get('pays')}")
-
-    print()
-
+for number in numbers:
+    print(number)
 print()
 
-shipping_label("Dr.","Spongebob","Squarepants","III",street_no = "123", street_name = "rue de blabla",code_postal="75000", commune = "Paris", pays = "France")
+for letter in letters:
+    print(letter)
+print()
+for extra_number in extra_numbers:
+    print(extra_number)
+print()
+
+for fruit in fruits:
+    print(fruit)
+print()
+for letter in name:
+    print(letter, end="")
+print()
+print()
+
+for key in my_dictionary.keys():
+    print(key)
+for value in my_dictionary.values():
+    print(value)
+for key, value in my_dictionary.items():
+    print(f"{key} : {value}")
+    print(key,value)
