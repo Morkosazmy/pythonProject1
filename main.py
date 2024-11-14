945d693fd6f376c9b606614edafa9ca0da2beb8c
morkosazmy <azmymorkos@cy-tech.fr>
2024-11-14 02:59:44 +0100

    N°34 Functions with KEYWORD arguments.

--- main.py
+++ main.py
@@ -1,46 +1,37 @@
-#Creating functions !
-from typing import override
+import time
 
 
-def happy_birthday():
-    print("Happiest of birthdays QUEER.")
-    print("Oh my bad i meant QUEEN hehe !")
-    for x in range(3):
-        print("CELEBRATION MOVES")
-def Dr_Han():
-    for x in range(3):
-        print("I AM A SURGEON !")
-    print("I AM A SURGEON DR HAN !")
-    print("I AM A SURGEON !")
+def count(end, start = 0): #put start at the end because it has a default and a non default variable must be addressed before the defaulted one
+    for x in range(start, end+1):
+        print(x)
+        time.sleep(1)
+    print("DONE !")
 
-def add(num1,num2):
-    x = num1 + num2
-    return x
-def subtract(x,y):
-    z = x - y
-    return z
-def multiply(x,y):
-    z = x * y
-    return z
-def divide(x,y):
-    z = x / y
-    return z
-a = add(2,3)
-b = subtract(2,3)
-c = multiply(2,3)
-d = divide(2,3)
 
-print(f"a = {a}\n"
-      f"b = {b}\n"
-      f"c = {c}\n"
-      f"d = {d:.2f}\n")
 
-def create_name(prenom, nom):
-    firstname = prenom.capitalize()
-    lastname = nom.upper()
-    return lastname + " " + firstname
-my_name = create_name("prénom","nom")
-#cant use my_name.create_name cause that's for methods not functions and we created a function.
+#count(10,5)
+#count(7)
 
-print(my_name)
-#print(add(3,5))
+def hello( greeting, title, firstname, lastname):
+    print(f"{greeting} {title}{firstname} {lastname} !")
+
+hello("Hello", lastname= "Squarepants",firstname="Spongebob", title= "Mr.")
+hello("Hello", "Mr.", "Spongebob", lastname="Squarepants")
+
+def net_price(list_price,discount = 0,tva = 20): # insert the discount & TVA in percentage without typing the % symbol
+    final_price = list_price * (1 - (discount/100)) * (1 + (tva/100))
+    return final_price
+
+#goofy code where i separate the number in 2s instead of just typing them
+def get_number(country, carrier, rest_of_number):
+    rest_of_number = list(rest_of_number)
+    rest_of_number = rest_of_number[0] , rest_of_number[1] , rest_of_number[2] , rest_of_number[3] , rest_of_number[4] , rest_of_number[5] , rest_of_number[6] , rest_of_number[7]
+    rest_of_number = f"{rest_of_number[0]}{rest_of_number[1]} {rest_of_number[2]}{rest_of_number[3]} {rest_of_number[4]}{rest_of_number[5]} {rest_of_number[6]}{rest_of_number[7]}"
+    return f"{country} {carrier} {rest_of_number}"
+
+print(get_number("+33", carrier= "1",rest_of_number= "23456789"))
+
+print(net_price(500))
+print(net_price(500,20))
+print(net_price(500,20,0))
+print(net_price(500,0,0))
