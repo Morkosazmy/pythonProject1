2837e3cf4132256b34fc766fb47b85c9251a3e76
morkosazmy <azmymorkos@cy-tech.fr>
2024-11-15 20:54:52 +0100

    i just needed to calculate something in a dumb way

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 
 import mymath
 
-x = int(input("Insert an integer : "))
-result = mymath.pi
-print(mymath.square(x))
-print(mymath.cube(x))
-print(mymath.circumference(x))
-print(mymath.area_of_circle(x))
-print(x)
-print(result)
+total = 0
+calculating = True
+while calculating:
+    minutes = int(input("Add minutes : "))
+    total += minutes * 60
+    print(total)
+    stop = input("Stop ? (y/n)")
+    if stop == "y" or stop == "Y":
+        calculating = False
+calculating = True
+while calculating:
+    seconds = int(input("Add seconds : "))
+    total += seconds
+    print(total)
+    stop = input("Stop ? (y/n)")
+    if stop == "y" or stop == "Y":
+        calculating = False
+
+time_sec = total % 60
+time_minute = int(total / 60) % 60
+time_hour = int(total / 3600)
+
+print(f"time : {time_hour}:{time_minute}:{time_sec}")
