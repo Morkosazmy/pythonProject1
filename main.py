1b7b6db7c74d551dc63664a14b346f43d93512fc
morkosazmy <azmymorkos@cy-tech.fr>
2024-11-15 04:58:28 +0100

    N°39 Case instead of lots of elif

--- main.py
+++ main.py
@@ -1,31 +1,11 @@
-def day_of_week(day):
-    match day:
-        case 1:
-            return "Sunday"
-        case 2:
-            return"Monday"
-        case 3:
-            return"Tuesday"
-        case 4:
-            return"Wednesday"
-        case 5:
-            return"Thursday"
-        case 6:
-            return"Friday"
-        case 7:
-            return"Saturday"
-        case _:
-            return"NOT A VALID DAY "
 
-print(day_of_week(1))
-print(day_of_week(9))
+import mymath
 
-def is_weekend(day):
-    match day:
-        case "Saturday" | "Sunday":
-            return True
-        case _:
-            return False
-print(is_weekend("Saturday"))
-print(is_weekend("Sunday"))
-print(is_weekend("Monday"))
+x = int(input("Insert an integer : "))
+result = mymath.pi
+print(mymath.square(x))
+print(mymath.cube(x))
+print(mymath.circumference(x))
+print(mymath.area_of_circle(x))
+print(x)
+print(result)
---
+++ mymath.py
@@ -0,0 +1,11 @@
+pi = 3.14159
+
+def square(x):
+    return x ** 2
+def cube(x):
+    return x ** 3
+def circumference(radius):
+    return radius * pi * 2
+
+def area_of_circle(radius):
+    return pi * square(radius)
