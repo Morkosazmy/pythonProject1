c2136012d5d12e99abb49a382a11303b004e01c2
morkosazmy <azmymorkos@cy-tech.fr>
2024-11-15 04:44:23 +0100

    N°38 LIST COMPREHENSIONS

--- main.py
+++ main.py
@@ -1,36 +1,31 @@
-#List comprehension
+def day_of_week(day):
+    match day:
+        case 1:
+            return "Sunday"
+        case 2:
+            return"Monday"
+        case 3:
+            return"Tuesday"
+        case 4:
+            return"Wednesday"
+        case 5:
+            return"Thursday"
+        case 6:
+            return"Friday"
+        case 7:
+            return"Saturday"
+        case _:
+            return"NOT A VALID DAY "
 
-doubles = [ x * 2 for x in range(1,11)]
-triples = [ x * 3 for x in range(1,11)]
-halves =  [ x / 2 for x in range(1,11)]
-squares = [ pow(x,2) for x in range(1,11)]
-#squares =[ (x ** 2 or x * x) for x in range(1,11)]
-foods = ["apple", "banana", "mango", "pineapple", "Orange"]
-print(foods)
-foods = [food.upper() for food in foods]
-# or
-fruits = [fruit.upper() for fruit in ["apple", "banana", "mango", "pineapple", "Orange"]]
-print(doubles)
-print(triples)
-print(halves)
-print(squares)
-print(foods)
-print(fruits)
+print(day_of_week(1))
+print(day_of_week(9))
 
-numbers = [1,2,3,4,5,6,-7,-8,-9,10,11,12,-13,14,-15,-16,17,18,19,20]
-positive_nums = [num for num in numbers if num >= 0]
-negative_nums = [num for num in numbers if num < 0]
-even_nums = [num for num in numbers if num % 2 == 0]
-odd_nums = [num for num in numbers if num % 2 == 1]
-
-print(numbers)
-print(positive_nums)
-print(negative_nums)
-print(even_nums)
-print(odd_nums)
-grades = [86, 45, 32, 68, 48, 98, 90, 2, 32, 100, 59, 60]
-print(grades)
-passing_grades = [num for num in grades if num >= 60]
-failing_grades = [num for num in grades if num <  60]
-print(passing_grades)
-print(failing_grades)
+def is_weekend(day):
+    match day:
+        case "Saturday" | "Sunday":
+            return True
+        case _:
+            return False
+print(is_weekend("Saturday"))
+print(is_weekend("Sunday"))
+print(is_weekend("Monday"))
