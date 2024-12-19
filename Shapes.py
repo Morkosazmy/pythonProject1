f0a4cad12944f4e461f3fdd4919ef8414409bc69
morkosazmy <azmymorkos@cy-tech.fr>
2024-12-19 03:07:36 +0100

    commit OOP python

--- Shapes.py
+++ Shapes.py
@@ -1,42 +1,35 @@
 from abc import ABC, abstractmethod
-import math
+
 class Shape:
-    def __init__(self, color, is_filled):
-        self.color = color
-        self.is_filled = is_filled
-    def describe(self):
-        print(f"It is {self.color} and {'filled' if self.is_filled else 'not filled'}")
+
+    @abstractmethod
+    def area(self):
+        pass
 
 class Circle(Shape):
-    def __init__(self, color,is_filled, radius):
-        super().__init__(color, is_filled)
+    def __init__(self,radius):
         self.radius = radius
-    def describe(self):
-        print(f"It is a {self.color} circle with an area of {math.pi * pow(self.radius,2):.2f} cm^2")
-        super().describe()
+
+    def area(self):
+        return 3.14 * self.radius ** 2
 
 class Square(Shape):
-    def __init__(self, color,is_filled, width):
-        super().__init__(color, is_filled)
-        self.width = width
-    def describe(self):
-        print(f"It is a {self.color} square with an area of {self.width * self.width:.2f} cm^2")
-        super().describe()
-        
-class Triangle(Shape):
-    def __init__(self, color, is_filled, width, height):
-        super().__init__(color, is_filled)
-        self.width = width
-        self.height = height
-    def describe(self):
-        print(f"It is a {self.color} Triangle with an area of {self.width * self.height:.2f} cm^2")
-        super().describe()
+    def __init__(self,side):
+        self.side = side
 
-class Rectangle(Shape):
-    def __init__(self, color, is_filled, width, length):
-        super().__init__(color, is_filled)
-        self.width = width
-        self.length = length
-    def describe(self):
-        print(f"It is a {self.color} Rectangle with an area of {self.width * self.length:.2f} cm^2")
-        super().describe()
+    def area(self):
+        return self.side ** 2
+
+class Triangle(Shape):
+    def __init__(self,base, height):
+        self.base = base
+        self.height = height
+
+    def area(self):
+        return self.base * self.height * 0.5
+
+
+Shapes = [Circle(4), Square(5), Triangle(6,7)]
+
+for shape in Shapes:
+    print(shape.area())
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""
 from http.client import CannotSendRequest
 
 import Shapes
@@ -12,3 +13,45 @@ circle.describe()
 square.describe()
 triangle.describe()
 rectangle.describe()
+
+"""
+from abc import ABC, abstractmethod
+
+class Shape:
+
+    @abstractmethod
+    def area(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self,radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius ** 2
+
+class Square(Shape):
+    def __init__(self,side):
+        self.side = side
+
+    def area(self):
+        return self.side ** 2
+
+class Triangle(Shape):
+    def __init__(self,base, height):
+        self.base = base
+        self.height = height
+
+    def area(self):
+        return self.base * self.height * 0.5
+
+class Pizza(Circle):
+    def __init__(self, topping, radius):
+        super().__init__(radius)
+        self.topping = topping
+
+
+Shapes = [Circle(4), Square(5), Triangle(6,7), Pizza("Pepperoni", 4)]
+
+for shape in Shapes:
+    print(f"Area of shape = {shape.area()}cm²")
