9e4d3300e72852397993ed5ec91b5a083c0b21a3
morkosazmy <azmymorkos@cy-tech.fr>
2024-12-20 03:21:29 +0100

    commit OOP python AGGREGATION

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
-class Animal:
-        alive = True
+#Aggregation
+class Library:
+    def __init__(self, name):
+        self.name = name
+        self.books = []
+    def add_book(self,book):
+        self.books.append(book)
 
-class Dog(Animal):
-    def speak(self):
-        print("WOOF!")
+    def list_books(self):
+        return [f"{book.title} by {book.author}" for book in self.books ]
 
-class Cat(Animal):
-    def speak(self):
-        print("MEOW!")
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
 
-class Duck(Animal):
-    def speak(self):
-        print("QUACK!")
+# We created a class for book and a library class as well
+# we need to create some books and print them.
+library = Library("NY Library")
 
-class Car:
-    alive = False
-    def speak(self):
-        print("HONK!")
+book1 = Book("Harry Potter", "J.K. Rowling")
+book2 = Book("The Hobbit", "J. R. R. Tolkein")
+book3 = Book("The Color Of Magic", "Terry Pratchet")
 
-animals = [Dog(),Cat(),Duck(),Car()]
+library.add_book(book1)
+library.add_book(book2)
+library.add_book(book3)
 
-for animal in animals:
-    animal.speak()
-    print(animal.alive, end="\n\n")
+print(library.name)
+print(library.list_books())
+
+for book in library.books:
+    print(f"Book's name : {book.title} , Book's Author : {book.author}")
