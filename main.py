3ddb29fc68f0912a3faf809805d75302d6c67f6c
morkosazmy <azmymorkos@cy-tech.fr>
2024-12-24 04:47:19 +0100

    commit OOP python STATIC METHODS

--- main.py
+++ main.py
@@ -1,54 +1,25 @@
-#Nested Classes
+#Static methods
 
-class Company:
-    class Employee:
-        def __init__(self, name, position):
-            self.name = name
-            self.position = position
+class Employee:
+    def __init__(self, name, position):
+        self.name = name
+        self.position = position
 
-        def get_details(self):
-            return f"{self.name} : {self.position}"
+    def get_info(self):
+        return f"{self.name} : {self.position}"
 
-    def __init__(self, company_name):
-        self.company_name = company_name
-        self.employees = []
+    @staticmethod
+    def is_valid_position(position):
+        valid_positions = ["Manager", "Cashier", "Cook", "Janitor"]
+        return position in valid_positions
 
-    def add_employee(self, name, position):
-        new_employee = self.Employee(name, position)
-        self.employees.append(new_employee)
+employee1 = Employee("Jeff","Cook")
+employee2 = Employee("Mark", "Cashier")
+employee3 = Employee("Mr. Krabs", "Manager")
 
+print(Employee.is_valid_position("Cook"))
+print(Employee.is_valid_position("cooker"))
 
-    def list_employees(self):
-        return [employee.get_details() for employee in self.employees]
-
-
-
-class None_profit:
-    class Employee:
-        pass
-    pass
-
-
-company1 = Company("Goodman and co.")
-company1.add_employee("Mark", "CEO")
-company1.add_employee("Mars", "Manager")
-company1.add_employee("Spongebob","Cook")
-company1.add_employee("Squidward","Cashier")
-print(company1.list_employees())
-
-
-company2 = Company("Chumbucket")
-company2.add_employee("Sheldon", "Manager")
-company2.add_employee("Computer", "Assistant")
-
-
-#1
-for employee in company1.list_employees():
-    print(employee)
-
-print()
-#2
-for employee in company2.employees:
-    print(employee.get_details())
-
-# Both 1 and 2 will do the same thing !
+print(employee1.get_info())
+print(employee2.get_info())
+print(employee3.get_info())
