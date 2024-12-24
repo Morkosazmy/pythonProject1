9c93954bfdcc0611b845c9084679e2e2560f5e2e
morkosazmy <azmymorkos@cy-tech.fr>
2024-12-24 19:26:36 +0100

    commit OOP python CLASS METHODS

--- main.py
+++ main.py
@@ -1,25 +1,40 @@
-#Static methods
+#Class Methods
 
-class Employee:
-    def __init__(self, name, position):
+class Student:
+
+    count = 0
+    total_gpa = 0
+    def __init__(self, name, gpa):
         self.name = name
-        self.position = position
+        self.gpa = gpa
+        Student.count += 1
+        Student.total_gpa += gpa
 
     def get_info(self):
-        return f"{self.name} : {self.position}"
+        return f"{self.name} : {self.gpa}"
 
-    @staticmethod
-    def is_valid_position(position):
-        valid_positions = ["Manager", "Cashier", "Cook", "Janitor"]
-        return position in valid_positions
+    @classmethod
+    def get_count(cls):
+        return f"The total number of students is : {cls.count}"
 
-employee1 = Employee("Jeff","Cook")
-employee2 = Employee("Mark", "Cashier")
-employee3 = Employee("Mr. Krabs", "Manager")
+    @classmethod
+    def get_average_gpa(cls):
+        return f"Average Gpa : {(cls.total_gpa / cls.count):.2f}"
+#print(Student.get_count())
 
-print(Employee.is_valid_position("Cook"))
-print(Employee.is_valid_position("cooker"))
+student1 = Student("Mark",3.9)
+student2 = Student("Marco",3.227)
+student3 = Student("Nick",3.23334)
+student4 = Student("pete",4.0)
 
-print(employee1.get_info())
-print(employee2.get_info())
-print(employee3.get_info())
+print(student1.get_info())
+print(student2.get_info())
+print(student3.get_info())
+print(student4.get_info())
+
+print(Student.get_count())
+
+
+print(Student.total_gpa)
+print(Student.get_count())
+print(Student.get_average_gpa())
