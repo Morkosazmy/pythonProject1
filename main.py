0b911e7096507d3835ce57ea7e77155fdb255549
morkosazmy <azmymorkos@cy-tech.fr>
2024-12-25 23:31:48 +0100

    commit OOP python REDUCE

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-#filter(function, collection) will return every element that pass a condition
-def is_passing(grade):
-    if grade >= 60:
-        return True
-    else:
-        return False
-grades = [56, 21, 48, 65, 98, 62, 28, 75, 41, 36, 22, 0, 8, 99, 100, 50, 51, 49, 86, 55, 67, 69]
+#reduce
+from functools import reduce
 
-passing_grades = list(filter(is_passing, grades))
-passing_grades2 = list(filter(lambda grade: grade >= 60, grades))
+def add(x,y):
+    return x + y
 
-print(passing_grades)
-print(passing_grades2)
+prices = [10.99, 18.99, 25.45, 29.99, 60, 70, 32 , 80]
+
+total1 = reduce(add, prices)
+total2 = reduce(lambda x, y: x + y, prices)
+
+print(f"${total1:.2f}")
+
+print(f"${total2:.2f}")
