81870e6126c2204e9aeb773c007533e4668171f4
morkosazmy <azmymorkos@cy-tech.fr>
2024-12-25 22:49:06 +0100

    commit OOP python FILTER

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
-#map(function, collection)
+#filter(function, collection) will return every element that pass a condition
+def is_passing(grade):
+    if grade >= 60:
+        return True
+    else:
+        return False
+grades = [56, 21, 48, 65, 98, 62, 28, 75, 41, 36, 22, 0, 8, 99, 100, 50, 51, 49, 86, 55, 67, 69]
 
-def c_to_f(temp):
-    return (temp * 9/5) + 32
+passing_grades = list(filter(is_passing, grades))
+passing_grades2 = list(filter(lambda grade: grade >= 60, grades))
 
-celsius_temps = [0.0, 32.0, 18.0, 55.0, 45.0, -8.0, -60.0, 100.0]
-
-fahrenheit_temps = list(map(c_to_f, celsius_temps))
-
-fahrenheit_temps2 = list(map(lambda temp: (temp * 9/5) + 32, celsius_temps))
-
-print(celsius_temps) # Prints a list of temps in Celsius
-
-print(fahrenheit_temps) # Prints a list of temps in fahrenheit
-
-for temp in fahrenheit_temps2:
-    print(temp)
-#Prints a temperature for each line
+print(passing_grades)
+print(passing_grades2)
