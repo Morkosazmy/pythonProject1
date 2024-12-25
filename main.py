60eb19a77614554741460de7cbd7342a2281ed38
morkosazmy <azmymorkos@cy-tech.fr>
2024-12-25 05:45:05 +0100

    commit OOP python LAMBDA FUNCTIONS

--- main.py
+++ main.py
@@ -1,58 +1,23 @@
-#@Property ( getter "@property", setter "@x.setter", deleter "@x.deleter" )
-from pprint import pprint
-from shutil import which
+#Lambda functions
 
+double = lambda x: x * 2
+add = lambda x, y: x + y
+max_value = lambda x, y: x if x > y else y
+min_value = lambda x, y: x if x < y else y
+is_even = lambda x: x % 2 == 0
+full_name = lambda first_name, last_name: first_name + " " + last_name
+#age_check = lambda x: f"{x} is over 18" if x >= 18 else "under age, ACCESS DENIED"
+age_check = lambda x: True if x >= 18 else False
 
-class Rectangle:
-    def __init__(self, width, height):
-        self._width = width
-        self._height = height
-        # the ' _ ' refers to the attributes are protected and shall only be used within the class itself
+print(double(3))
+print(double(4))
+print()
 
-    @property
-    def width(self):
-        return f"{self._width:.1f} cm"
-
-    @property
-    def height(self):
-        return f"{self._height:.1f} cm"
-
-    @width.setter
-    def width(self, new_width):
-        if new_width > 0:
-            self._width = new_width
-        else:
-            print("Width must be greater then zero !")
-
-    @height.setter
-    def height(self, new_height):
-        if new_height > 0:
-            self._height = new_height
-        else:
-            print("Height must be greater than zero")
-
-    @width.deleter
-    def width(self):
-        del self._width
-        print("Width has been deleted")
-
-    @height.deleter
-    def height(self):
-        del self._height
-        print("Height has been deleted")
-
-rectangle = Rectangle(3, 4)
-
-print(rectangle.width)
-print(rectangle.height)
-
-rectangle.width = 5
-rectangle.height = 6
-
-pprint('')
-
-print(rectangle.width)
-print(rectangle.height)
-
-del rectangle.width
-del rectangle.height
+print(add(3,2))
+print(max_value(1,85))
+print(min_value(1,85))
+print(is_even(5))
+print(is_even(100))
+print(full_name("Mark", "Zuckerberg"))
+print(age_check(5))
+print(age_check(23))
