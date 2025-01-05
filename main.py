179becfcc82b09921130a8fd7ff54c6216eec5a7
morkosazmy <azmymorkos@cy-tech.fr>
2025-01-05 01:07:26 +0100

    commit python => FILE WRITING (.txt, .json, .csv, & collections)

--- main.py
+++ main.py
@@ -1,43 +1,86 @@
-#File detection
+# writing files ( a, x, r, w )
 
-import os
 
-#direct connection:
-file_path = "test.txt"
 
-#indirect connection:
-file_path2 = "C:/Users/morko/PycharmProjects/pythonProject/.venv/test.txt"
-file_path3 = "C:/Users/morko/PycharmProjects/pythonProject/test.txt"
-file_path1 = "C:/Users/morko/OneDrive/Bureau/New Document texte (11).txt"
-path_dir = "C:/Users/morko/OneDrive/Bureau/Corrigé proba alternance"
-if os.path.exists(file_path2):
-    print(f"found the '{file_path2}' file")
-else:
-    print("file not found")
+# w to write or create a new one    ( it will override the file )
+# x to create a new one and if it exists it'll give an error and won't create a second one
 
-if os.path.exists(file_path):
-    print(f"found the '{file_path}' file")
-else:
-    print("file not found")
+"""
+txt_data = "I love you !"
 
-if os.path.exists(file_path3):
-    print(f"found the '{file_path3}' file")
-else:
-    print("file not found")
+file_path = "C:/Users/morko/OneDrive/Bureau/python_test.txt"
 
-if os.path.exists(file_path1):
-    print(f"found the '{file_path1}' file")
-else:
-    print("file not found")
+try:
+    with open(file_path, "w" ) as file:
+        file.write(txt_data)
+        print(f"The file '{file_path}' was created")
+except FileExistsError:
+    print("The file exists already")
 
-if os.path.exists(file_path1):
-    print(f"found the '{file_path1}' file")
-else:
-    print("file not found")
+# a to append data to an existing file
 
-if os.path.isfile(path_dir):
-    print(f"found the '{path_dir}' file")
-elif os.path.isdir(path_dir): #no extension (ex: .txt .pdf etc...)
-    print(f"found the '{path_dir}' directory")
-else:
-    print("File/Directory not found")
+
+try:
+    with open(file_path, "a" ) as file:
+        file.write("\n" + txt_data)
+        print(f"The file '{file_path}' was created")
+except FileExistsError:
+    print("The file exists already")
+
+"""
+import json
+
+
+"""
+# Collections
+employees = ["Patrick", "Spongebob", "Eugene", "Squidward", "Sally"]
+
+file_path = "C:/Users/morko/OneDrive/Bureau/python_test.txt"
+
+try:
+    with open(file_path, "w" ) as file:
+        for employee in employees:
+            file.write(" " + employee)
+        print(f"The file '{file_path}' was created ")
+except FileExistsError:
+    print("The file exists already ")
+"""
+
+"""
+#json
+employee = {
+    "name" : "Spongebob",
+    "age" : 30,
+    "position" : "chef"
+}
+
+
+file_path = "C:/Users/morko/OneDrive/Bureau/python_test.json"
+
+try:
+    with open(file_path, "w" ) as file:
+        json.dump(employee, file, indent = 4)
+        #for employee in employees:
+        #    file.write(" " + employee)
+        print(f"The json file '{file_path}' was created ")
+except FileExistsError:
+    print("The file exists already ")
+
+"""
+
+import csv
+
+file_path = "C:/Users/morko/OneDrive/Bureau/ING INFO/python_tesst.csv"
+employees = [["name", "age", "job"],
+             ["Spongebob", 30, "cook"],
+             ["Patrick", 38, "unemployed"],
+             ["Eugene", 56, "manager"],
+             ["Squidward", 40, "cashier"]]
+try:
+    with open(file_path, "w") as file:
+        writer = csv.writer(file)
+        for row in employees:
+            writer.writerow(row)
+        print(f"The CSV file '{file_path}' was created ")
+except FileExistsError:
+    print("The file exists already ")
