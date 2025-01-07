f2de1c1848857d40061419e4633b2e96c77563ae
morkosazmy <azmymorkos@cy-tech.fr>
2025-01-07 02:12:54 +0100

    commit python => DATE AND TIME

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
-#Date And Time
+#Alarm clock
 
-import datetime
-from time import strftime
+import pygame   #sound effects
+import time     #time module
+import datetime #time in strings
 
-date = datetime.date(2025, 1, 7)
-day = datetime.date.today()
+def set_alarm(alarm_time):
+    print(f"Alarm set for {alarm_time} ")
+    sound_file = "C:/Users/morko/PycharmProjects/pythonProject/Between The Spaces - The Soundlings.mp3"
 
-time = datetime.time(22, 12, 00)
-now = datetime.datetime.now()
+    is_running = True
 
-now2 = now.strftime("%H:%M:%S / %d-%m-%Y")
+    while is_running:
+        current_time = datetime.datetime.now().strftime("%H:%M:%S")
+        print(current_time)
 
-target_datetime = datetime.datetime(2025, 1, 7, 00, 00, 1)
+        if (current_time == alarm_time):
+            print("WAKE UP, ITS THE FIRST OF THE MONTH")
+            time.sleep(0.5)
+            is_running = False
+            pygame.mixer.init()
+            pygame.mixer.music.load(sound_file)
+            pygame.mixer.music.play()
 
-current_datetime = datetime.datetime.now()
+            while pygame.mixer.music.get_busy():
+                time.sleep(1)
 
-print(date)
-print(day)
-print(time)
-print(now)
-print(now2)
+        time.sleep(1)
 
-if target_datetime < current_datetime:
-    print("Target date time has passed")
-else:
-    print("Target date time has NOT passed")
+
+if __name__ == "__main__":
+    alarm_time = input("Enter the time for the alarm (HH:MM:SS) : ")
+    set_alarm(alarm_time)
