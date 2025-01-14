3ff8c093fd84fb951537ee26b60c65c1f8a08431
morkosazmy <azmymorkos@cy-tech.fr>
2025-01-14 02:28:31 +0100

    commit python => MULTITHREADING

--- main.py
+++ main.py
@@ -1,33 +1,53 @@
-#Alarm clock
+#Multithreading
 
-import pygame   #sound effects
-import time     #time module
-import datetime #time in strings
-
-def set_alarm(alarm_time):
-    print(f"Alarm set for {alarm_time} ")
-    sound_file = "C:/Users/morko/PycharmProjects/pythonProject/Between The Spaces - The Soundlings.mp3"
-
-    is_running = True
-
-    while is_running:
-        current_time = datetime.datetime.now().strftime("%H:%M:%S")
-        print(current_time)
-
-        if (current_time == alarm_time):
-            print("WAKE UP, ITS THE FIRST OF THE MONTH")
-            time.sleep(0.5)
-            is_running = False
-            pygame.mixer.init()
-            pygame.mixer.music.load(sound_file)
-            pygame.mixer.music.play()
-
-            while pygame.mixer.music.get_busy():
-                time.sleep(1)
-
-        time.sleep(1)
+import threading
+import time
 
 
-if __name__ == "__main__":
-    alarm_time = input("Enter the time for the alarm (HH:MM:SS) : ")
-    set_alarm(alarm_time)
+def take_out_trash():
+    print("You are taking out the trash")
+    time.sleep(10)
+    print("You took out the trash")
+
+def do_laundry():
+    print("You are doing the laundry")
+    time.sleep(10)
+    print("You did the laundry")
+
+def wash_dishes():
+    print("You are washing the dishes")
+    time.sleep(4)
+    print("You washed the dishes")
+
+def get_mail():
+    print("You are getting the mail")
+    time.sleep(6)
+    print("You got the mail")
+
+def walk_dog(first_name, last_name):
+    print(f"You are walking the dog {first_name} {last_name}")
+    time.sleep(1)
+    print(f"You walked the dog {first_name} {last_name}")
+
+chore1 = threading.Thread(target=take_out_trash)
+chore1.start()
+
+chore2 = threading.Thread(target=do_laundry)
+chore2.start()
+
+chore3 = threading.Thread(target=wash_dishes)
+chore3.start()
+
+chore4 = threading.Thread(target=get_mail)
+chore4.start()
+
+chore5 = threading.Thread(target=walk_dog, args=("scooby","doo"))
+chore5.start()
+
+chore1.join()
+chore2.join()
+chore3.join()
+chore4.join()
+chore5.join()
+
+print("All chores are finished")
