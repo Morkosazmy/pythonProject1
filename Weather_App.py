182a61a595c32f5b2ab2ad1e65bec83cc807be25
morkosazmy <azmymorkos@cy-tech.fr>
2025-02-08 23:24:27 +0100

    commit python => PyQt5 WEATHER APP

--- Weather_App.py
+++ Weather_App.py
@@ -77,7 +77,7 @@ class WeatherApp(QWidget):
         self.get_weather_button.clicked.connect(self.get_weather)
 
     def get_weather(self):
-        api_key = "8560d63f74402e629c38d69e70c22b1b"
+        api_key = "API KEY SHOULD BE PUT HERE"
         city = self.city_input.text()
         url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
         try:
